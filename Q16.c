835dcc2b4f6a1409cb3dfe56c1409b9785b1e755
dudamesdc <149789767+dudamesdc@users.noreply.github.com>
2024-04-01 12:41:52 -0300

    Update Q16.c

--- Q16.c
+++ Q16.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-float compare(const void *a,const void *b){
-   return *(int *) a-*(int *)b 
+
+// Função de comparação para qsort
+int compare(const void *a, const void *b) {
+    float c= (const float *)a-(const float *)b;
+    if (c > 0) return 1;
+    if (c < 0) return -1;
+    return 0;
 }
-int main()
-{
+
+int main() {
     float *num;
-    int tam, i;
+    int tam;
+
+    // Lendo o número de valores
     scanf("%d", &tam);
-    num = (float *)(malloc(tam*sizeof(float)));
-    for(int i=0; i<tam; i++){
+
+    // Alocando memória para o array de floats
+    num = (float *)malloc(tam * sizeof(float));
+    if (num == NULL) {
+        printf("Erro ao alocar memória.\n");
+        return 1; // Encerrando o programa com código de erro
+    }
+
+    // Lendo os valores
+    for (int i = 0; i < tam; i++) {
         scanf("%f", &num[i]);
     }
-    for (int j=0; j<tam-1; j++){
-        for (int i=0; i<tam-1; i++){
-            qsort(array, tam, sizeof(float), compare(num[i],num[i+1])
-        }
+
+    // Ordenando os valores utilizando qsort
+    qsort(num, tam, sizeof(float), compare);
+
+    // Imprimindo os valores ordenados
+    for (int i = 0; i < tam; i++) {
+        printf("%.3f\n", num[i]);
     }
-    for (int i=0; i<tam; i++){
-        printf("%.3f\n",num[i]);
-    }
-    free (num);
+
+    // Liberando a memória alocada
+    free(num);
+
     return 0;
 }
