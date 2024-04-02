bdaefc3a4c10fb24e02a57337da19232cd47a011
thamirego <93604132+thamirego@users.noreply.github.com>
2024-04-02 17:11:00 -0300

    Create Q11.c

---
+++ Q11.c
@@ -0,0 +1,5 @@
+
+Para char x[4], x+1 = 4093, x+2 = 4094 e x+3 = 4095.(variaveis do tipo char ocupam 1 byte)
+Para int x[4], x+1 = 4096, x+2 = 4100 e x+3 = 4104.(variaveis do tipo int ocupam 2 bytes)
+Para float x[4], x+1 = 4096, x+2 = 4100 e x+3 = 4104.(variaveis do tipo float ocupam 4 bytes)
+Para double x[4], x+1 = 4100, x+2 = 4108 e x+3 = 4116.(variaveis do tipo double ocupam 8 bytes)
